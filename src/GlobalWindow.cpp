c523f03fd88bba9b455c4f6a7579aed1af460ab2
bustlingbungus <cadens@usf.edu>
2025-01-26 11:35:28 -0500

    Created project template

--- src/GlobalWindow.cpp
+++
@@ -1,9 +0,0 @@
-#include "GlobalWindow.hpp"
-
-// create the window pointer 
-const std::shared_ptr<LWindow> gWindow = std::make_shared<LWindow>(WND_WIDTH, WND_HEIGHT, wndName);
-
-/* Sets the title of the global window */
-void SetWindowTitle(std::string title) {
-    gWindow->setName(title);
-}
---
+++ src/Init.cpp
@@ -0,0 +1,12 @@
+#include <BungusEngine.hpp>
+
+/*
+ * ADD YOUR INITIALISATION CODE HERE
+ *
+ * This should be code for things like adding scenes - this function is only called
+ * once, before the window is opened.
+ */
+void Init()
+{
+    
+}
